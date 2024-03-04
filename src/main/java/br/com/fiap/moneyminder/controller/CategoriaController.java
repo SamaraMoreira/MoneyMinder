e7c42bf47848a5128a4a0982c6dfe16ad6abc7b2
SamaraMoreira <samaramoreira000@gmail.com>
2024-03-04 08:59:48 -0300

    Realizando o Post e Put

--- src/main/java/br/com/fiap/moneyminder/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/moneyminder/controller/CategoriaController.java
@@ -4,21 +4,31 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.moneyminder.model.Categoria;
 
 
 
 @Controller
+@RestController
+@RequestMapping("categoria")
 public class CategoriaController {
 
     Logger log = LoggerFactory.getLogger(getClass());
     List<Categoria> repository = new ArrayList<>();
 
-    @ResponseBody
+    @GetMapping
     public List<Categoria> index() {
         return repository;
     }
@@ -54,6 +64,45 @@ public class CategoriaController {
             return ResponseEntity.notFound().build();
 
         return ResponseEntity.ok(optionalCategoria.get());
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object>destroy(@PathVariable Long id){
+        log.info("Apagando categoria{}", id);
+
+        var optionalCategoria = repository
+            .stream()
+            .filter(c -> !c.id().equals(id))
+            .findFirst();
+
+        if(optionalCategoria.isEmpty())
+            return ResponseEntity.notFound().build();
+
+
+        repository.remove(optionalCategoria.get());
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Object> update(@PathVariable Long id, @RequestBody Categoria categoria){
+
+        log.info("Atualizando a categoria id{} para {}", id, categoria);
+
+        var optionalCategoria = repository
+            .stream()
+            .filter(c -> c.id().equals(id))
+            .findFirst();
+
+        if(optionalCategoria.isEmpty())
+            return ResponseEntity.notFound().build();
+        
+        var categoriaEncontrada = optionalCategoria.get();
+        var categoriaAtualizada = new Categoria(id, categoria.nome(), categoria.icone());
+        repository.remove(categoriaEncontrada);
+        repository.add(categoriaAtualizada);
+        return ResponseEntity.ok().body(categoriaAtualizada);
+
     }
 
 }
--- src/main/java/br/com/fiap/moneyminder/model/Categoria.java
+++ src/main/java/br/com/fiap/moneyminder/model/Categoria.java
@@ -1,9 +1,14 @@
 package br.com.fiap.moneyminder.model;
 
-public record Categoria(
-    Long id,
-    String nome,
-    String icone
-) {
-    
+import java.util.Random;
+
+public record Categoria(Long id, String nome, String icone) {
+
+    public Categoria(Long id, String nome, String icone){
+        var key = (id != null) ? id : Math.abs( new Random().nextLong() );
+        this.id = key;
+        this.icone = icone;
+        this.nome = nome;
+    }
+
 }
