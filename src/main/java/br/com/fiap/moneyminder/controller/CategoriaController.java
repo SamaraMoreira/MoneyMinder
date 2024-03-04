4fd837c99f5a52f3d73289e810c13cc646f52d51
Samara Moreira <128256951+SamaraMoreira@users.noreply.github.com>
2024-03-04 07:52:56 -0300

    Update CategoriaController.java

--- src/main/java/br/com/fiap/moneyminder/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/moneyminder/controller/CategoriaController.java
@@ -6,8 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.fiap.moneyminder.model.Categoria;
@@ -20,20 +18,42 @@ public class CategoriaController {
     Logger log = LoggerFactory.getLogger(getClass());
     List<Categoria> repository = new ArrayList<>();
 
-    @RequestMapping(method = RequestMethod.GET , path = "/categoria", produces ="application/json")
     @ResponseBody
     public List<Categoria> index() {
         return repository;
     }
 
-
-    
-    @RequestMapping(method=RequestMethod.POST, path = "/categoria", produces ="application/json", consumes = "application/json")
-    @ResponseBody
-    public void create(@RequestBody Categoria categoria){
-        log.info("cadastrando categoria");
+    @PostMapping
+    // @ResponseStatus(code = HttpStatus.CREATED)
+    public ResponseEntity<Categoria> create(@RequestBody Categoria categoria) {
+        log.info("cadastrando categoria: {}", categoria);
         repository.add(categoria);
-
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(categoria);
     }
-    
+
+    @GetMapping("{id}")
+    public ResponseEntity<Categoria> get(@PathVariable Long id) {
+        log.info("Buscar por id: {}", id);
+
+        // for (Categoria categoria : repository ){
+        // if (categoria.id().equals(id)){
+        // return ResponseEntity
+        // .status(HttpStatus.OK)
+        // .body(categoria);
+        // }
+        // }
+
+        var optionalCategoria = repository
+                .stream()
+                .filter(c -> c.id().equals(id))
+                .findFirst();
+
+        if (optionalCategoria.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(optionalCategoria.get());
+    }
+
 }
